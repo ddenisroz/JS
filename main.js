ca7d5113bbd0e43c116d1307bbd3aac4fa2bd15c
Denis R <rozenkod312@gmail.com>
2023-01-24 00:03:24 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+/*alert('Привет посетитель');
+alert('Еще одно уведомление');
+alert(2+2);
+let number;
+number = 10;
+console.log(number+1);
+console.log('ya molodec');
+let userName = 'Denis';
+let userAge = 26;
+let userHeight = 178;
+console.log(userName + 'is' + userAge + 'years old' + 'height' + userHeight);
+*/
+let a = 4;
+console.log(++a);
